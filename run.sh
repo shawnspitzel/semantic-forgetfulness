aa2e9f0cdd81c959b2020d8bd671ce65269be9d9
ShawnSpitzel <158654739+ShawnSpitzel@users.noreply.github.com>
2026-03-17 15:33:11 -0400

    fix: add en_core_web_sm installation

--- run.sh
+++ run.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 # Usage: bash run.sh
 
-set -euo pipefail
+set -eo pipefail
 
 PARTITION="general-gpu"
 GRES="gpu:1"
 MEM="16G"
 TIME="02:00:00"
+
+export PATH=$HOME/miniconda3/bin:$PATH
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+conda activate sf-mvp
+cd /home/sps22006/semantic-forgetfulness/
+
+python -m spacy download en_core_web_sm
 
 srun --pty \
   --partition="$PARTITION" \
