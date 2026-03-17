cd6dcb08bf16415577d73902ec7bbc3a12a9051e
sps22006 <sps22006@login6.storrs.hpc.uconn.edu>
2026-03-17 15:32:13 -0400

    fix: fix dependencies

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 # Usage: bash run.sh
 
-set -euo pipefail
+set -eo pipefail
 
 PARTITION="general-gpu"
 GRES="gpu:1"
 MEM="16G"
 TIME="02:00:00"
+
+export PATH=$HOME/miniconda3/bin:$PATH
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+conda activate sf-mvp
+cd /home/sps22006/semantic-forgetfulness/
 
 srun --pty \
   --partition="$PARTITION" \
